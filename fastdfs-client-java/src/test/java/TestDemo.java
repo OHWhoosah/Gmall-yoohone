07f48dcd0514559bab0e30ec549f861993af492e
Saukes <saukes@163.com>
2020-04-25 19:22:11 +0800

    添加message：Hello MLDNJava ！！！

--- fastdfs-client-java/src/test/java/TestDemo.java
+++ fastdfs-client-java/src/test/java/TestDemo.java
@@ -1,6 +1,6 @@
 public class TestDemo {
     public static void main(String[] args) {
         System.out.println("Hello World !!!");
-        System.out.println("Hello MLDN !!!");
+        System.out.println("Hello MLDNJava !!!");
     }
 }
