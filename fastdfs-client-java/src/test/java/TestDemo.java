eb84745e0da27de7cf15ca7ff4ace259aed60b8d
yoohone <yangdong19891130@163.com>
2020-04-25 19:26:10 +0800

    修改message：我爱中国 中国万岁

--- fastdfs-client-java/src/test/java/TestDemo.java
+++ fastdfs-client-java/src/test/java/TestDemo.java
@@ -2,5 +2,6 @@ public class TestDemo {
     public static void main(String[] args) {
         System.out.println("Hello World !!!");
         System.out.println("Hello Atguigu !!!");
+        System.out.println("我爱中国，中国万岁!");
     }
 }
