03d2c37219ea6cd195a9106641864155e283db65
yoohone <yangdong19891130@163.com>
2020-04-25 20:44:30 +0800

    修改message：吾爱中国，

--- fastdfs-client-java/src/test/java/TestDemo.java
+++ fastdfs-client-java/src/test/java/TestDemo.java
@@ -2,6 +2,6 @@ public class TestDemo {
     public static void main(String[] args) {
         System.out.println("Hello World !!!");
         System.out.println("我爱中国，中国万岁!");
-        System.out.println("我爱中国 !!!");
+        System.out.println("吾爱中国，自强不息，铮铮铁骨!");
     }
 }
