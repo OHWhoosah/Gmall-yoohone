3796f8c2913759c6853289164b2c157216a96e64
Saukes <saukes@163.com>
2020-04-27 20:41:05 +0800

    添加message：Hello MLDNJava ！！！

--- fastdfs-client-java/src/test/java/TestDemo.java
+++ fastdfs-client-java/src/test/java/TestDemo.java
@@ -3,5 +3,6 @@ public class TestDemo {
         System.out.println("Hello World !!!");
         System.out.println("我爱中国，中国万岁!");
         System.out.println("吾爱中国，自强不息，铮铮铁骨!");
+        System.out.println("提交rebase分支！");
     }
 }
